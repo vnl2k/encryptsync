1ba171d048d7932fa2d2fd51f39ed40a3b62d2d7
nikvaklev <dazimov@gmail.com>
2018-06-05 18:16:09 +0100

    Added prettier and eslint

--- encrypt/encrypt.js
+++ encrypt/encrypt.js
@@ -1,85 +1,89 @@
-"use strict"
+"use strict";
 
-const fs = require('fs'),
-	{ spawn, spawnSync} = require('child_process'),
-	nodemon = require('nodemon'),
-	EMAIL = process.argv[2],
-	DESTINATION_PATH = process.argv[3] || (process.env.HOME + "/Dropbox"),
-	CWD = process.cwd();
+const fs = require("fs"),
+  { spawn } = require("child_process"),
+  nodemon = require("nodemon"),
+  EMAIL = process.argv[2],
+  DESTINATION_PATH = process.argv[3] || process.env.HOME + "/Dropbox",
+  CWD = process.cwd();
 
-const ERROR_LOG = "error-log.txt"
+const ERROR_LOG = "error-log.txt";
 
 function logMessage(message) {
-	fs.appendFile(ERROR_LOG, ["[", Date(), "] ", message, "\n"].join(""), 'utf8',  (err) => {
-	  if (err) throw err;
-	});
-} 
+  fs.appendFile(
+    ERROR_LOG,
+    ["[", Date(), "] ", message, "\n"].join(""),
+    "utf8",
+    err => {
+      if (err) throw err;
+    }
+  );
+}
 
 if (EMAIL == undefined) {
-	console.log("Please provide recepient email, e.g.\n npm run monitor john.smith@mail.com")
-	process.exit()
+  console.log(
+    "Please provide recepient email, e.g.\n npm run monitor john.smith@mail.com"
+  );
+  process.exit();
 }
 
 // This call to nodemon initialises nodemon within the script
 nodemon({});
 
-var FILES = [], gpg;
+var FILES = [],
+  gpg;
 
-process.on('SIGUSR2', function () {
+process.on("SIGUSR2", function() {
   // gracefulShutdown(function () {
   //   process.kill(process.pid, 'SIGUSR2');
   // });
 });
 
+nodemon
+  .on("start", function() {
+    if (gpg === undefined || gpg.signalCode === "SIGUSR2") {
+      gpg = spawn("gpg", ["-e", "-r", EMAIL, "--multifile"]);
+      gpg
+        .on("error", err => {
+          logMessage(err);
+        })
+        .on("close", (code, signal) => {})
+        .on("exit", (code, signal) => {
+          if (FILES !== undefined && FILES.length > 0)
+            FILES.forEach(f => {
+              let file_name = f + ".gpg";
+              let target_file = file_name.replace(CWD, DESTINATION_PATH);
+              let relative_path = file_name.replace(CWD, "");
 
-nodemon.on('start', function() {
-	if (gpg === undefined || gpg.signalCode === "SIGUSR2") {
-		gpg = spawn('gpg', ['-e', '-r', EMAIL, '--multifile']); 
-		gpg
-			.on('error', (err) => {
-			  logMessage(err);
-			})
-			.on('close', (code, signal) => {
-			})
-			.on('exit', (code, signal) => {
+              fs.stat(file_name, (err, stats) => {
+                if (err) return logMessage(err);
+                if (stats !== undefined && stats.isFile()) {
+                  fs.copyFile(file_name, target_file, err => {
+                    if (err) return logMessage(err);
 
-				 if (FILES !== undefined && FILES.length > 0) FILES.forEach(f => {
-			  	let file_name = f+".gpg";
-			  	let target_file = file_name.replace(CWD, DESTINATION_PATH)
-			  	let relative_path = file_name.replace(CWD, "")
-			 		
-					fs.stat(file_name, (err, stats) => {
-						if (err) return logMessage(err)
-						if (stats !== undefined && stats.isFile()) {
-				  		fs.copyFile(file_name, target_file, (err) => {
-							  if (err) return logMessage(err);
+                    fs.unlink(file_name, err => {
+                      if (err) logMessage(err);
+                      console.log(`moving: ${relative_path}\n`);
+                    });
+                  });
+                }
+              });
+            });
+        });
 
-							  fs.unlink(file_name, (err) => {
-								  if (err) logMessage(err);
-								  console.log(`moving: ${relative_path}\n`)
-								});
+      gpg.stdout.on("end", () => {
+        // END event is emitted when gpg is done encrypting the files
+      });
+    }
 
-							});
-			  		}
-			  	})
-			 	})
-			})
-	
-		gpg.stdout.on('end', () => {
-			// END event is emitted when gpg is done encrypting the files
-		});
-	
-	}
+    FILES = [];
+  })
+  .on("restart", files => {
+    FILES = files || [];
 
-
-	FILES = []
-		
-}).on('restart', files => {
-	FILES = files || [];
-
-  gpg.stdin.write(files.map(f => f+"\n").join(""));
-
-}).on('quit', function () {
-  console.log('App has quit');
-  process.exit();
-});
+    gpg.stdin.write(files.map(f => f + "\n").join(""));
+  })
+  .on("quit", function() {
+    console.log("App has quit");
+    process.exit();
+  });
