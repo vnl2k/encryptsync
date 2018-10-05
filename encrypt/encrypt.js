2e8d3f49e681a3518667dbcaa017aa931d59ada3
vnl2k <38137627+vnl2k@users.noreply.github.com>
2018-10-05 16:41:17 +0100

    v 0.1.0: refactored encrypt js
    Added mocha tests

--- encrypt/encrypt.js
+++ encrypt/encrypt.js
@@ -3,11 +3,8 @@
 const fs = require("fs"),
   { spawn } = require("child_process"),
   nodemon = require("nodemon"),
-  EMAIL = process.argv[2],
-  DESTINATION_PATH = process.argv[3] || process.env.HOME + "/Dropbox",
-  CWD = process.cwd()
-
-const ERROR_LOG = "error-log.txt";
+  Path = require("path"),
+  ERROR_LOG = "error-log.txt";
 
 function logMessage(message) {
   fs.appendFile(
@@ -20,70 +17,89 @@ function logMessage(message) {
   );
 }
 
-if (EMAIL == undefined) {
-  console.log(
-    "Please provide recepient email, e.g.\n npm run monitor john.smith@mail.com"
-  );
-  process.exit();
+function initGPG(email) {
+  let gpg = spawn("gpg", ["-e", "-r", email, "--multifile"]);
+
+  gpg.on("error", err => logMessage(err));
+  // .on("close", (code, signal) => {console.log("close event")});
+
+  // gpg.stdout.on("end", () => {
+  //   // END event is emitted when gpg is done encrypting the files
+  // });
+
+  return gpg;
 }
 
-// This call to nodemon initialises nodemon within the script
-nodemon({});
+function encryptFiles(gpg, files, source_path, target_path, callback) {
+  gpg.on("exit", (code, signal) => {
+    let fLen = files.length;
+    let count = [];
+    let callbackCheck = f => {
+      if (callback !== undefined) {
+        count.push(f);
+        if (count.length === fLen) callback(count);
+      }
+    };
 
-var FILES = [],
-  gpg;
+    if (fLen > 0)
+      files.forEach(f => {
+        let file_name = f + ".gpg";
+        let target_file = file_name.replace(source_path, target_path);
+        let relative_path = file_name.replace(source_path, "");
 
-process.on("SIGUSR2", function() {
-  // gracefulShutdown(function () {
-  //   process.kill(process.pid, 'SIGUSR2');
-  // });
-});
+        fs.stat(file_name, (err, stats) => {
+          if (err) {
+            callbackCheck(target_file);
+            return logMessage(err);
+          }
 
-nodemon
-  .on("start", function() {
-    if (gpg === undefined || gpg.signalCode === "SIGUSR2") {
-      gpg = spawn("gpg", ["-e", "-r", EMAIL, "--multifile"]);
-      gpg
-        .on("error", err => {
-          logMessage(err);
-        })
-        .on("close", (code, signal) => {})
-        .on("exit", (code, signal) => {
-          if (FILES !== undefined && FILES.length > 0)
-            FILES.forEach(f => {
-              let file_name = f + ".gpg";
-              let target_file = file_name.replace(CWD, DESTINATION_PATH);
-              let relative_path = file_name.replace(CWD, "");
+          if (stats !== undefined && stats.isFile()) {
+            fs.copyFile(file_name, target_file, err => {
+              if (err) {
+                callbackCheck(target_file);
+                return logMessage(err);
+              }
 
-              fs.stat(file_name, (err, stats) => {
-                if (err) return logMessage(err);
-                if (stats !== undefined && stats.isFile()) {
-                  fs.copyFile(file_name, target_file, err => {
-                    if (err) return logMessage(err);
-
-                    fs.unlink(file_name, err => {
-                      if (err) logMessage(err);
-                      console.log(`moving: ${relative_path}\n`);
-                    });
-                  });
-                }
+              fs.unlink(file_name, err => {
+                if (err) logMessage(err);
+                console.log(`moving: ${relative_path}\n`);
+                callbackCheck(target_file);
               });
             });
+          }
         });
-
-      gpg.stdout.on("end", () => {
-        // END event is emitted when gpg is done encrypting the files
       });
-    }
-
-    FILES = [];
-  })
-  .on("restart", files => {
-    FILES = files || [];
-
-    gpg.stdin.write(files.map(f => f + "\n").join(""));
-  })
-  .on("quit", function() {
-    console.log("App has quit");
-    process.exit();
   });
+
+  // encrypt the files
+  gpg.stdin.write(files.map(f => f + "\n").join(""));
+  gpg.stdin.end();
+}
+
+module.exports = {
+  encryptFiles: encryptFiles,
+  initGPG: initGPG,
+  logMessage: logMessage,
+  monitor: (email, source_path, target_path) => {
+
+    let GPG,
+      conf = JSON.parse(fs.readFileSync("./nodemon.json"));
+
+    source_path = Path.resolve(source_path);
+    target_path = Path.resolve(target_path);
+    conf.watch = [source_path];
+
+    // This call to nodemon initialises nodemon within the script
+    nodemon({})
+      .on("start", () => {
+        GPG = initGPG(email);
+      })
+      .on("restart", files => {
+        encryptFiles(GPG, files, source_path, target_path);
+      })
+      .on("quit", function() {
+        console.log("App has quit");
+        process.exit(0);
+      });
+  }
+};
---
+++ encrypt/tests/encrypt.js
@@ -0,0 +1,91 @@
+"use strict";
+
+const assert = require("assert"),
+  Path = require("path"),
+  fs = require("fs"),
+  { encryptFiles, initGPG, logMessage } = require("../encrypt.js");
+
+const SUORCE_PATH = Path.resolve("./tests/source"),
+  TARGET_PATH = Path.resolve("./tests/target"),
+  resolver = name => Path.resolve("./tests/source/" + name),
+  FILES = [
+    resolver("a.txt"),
+    resolver("b.pdf"),
+    resolver("c.txt"),
+    resolver("d.doc"),
+    resolver("new-folder/a.doc")
+  ];
+
+// The account password is "test
+
+describe("Testing sync-app", function() {
+  it("should encrypt a single file", function(done) {
+    let gpg = initGPG("test@example.com");
+
+    encryptFiles(
+      gpg,
+      FILES.slice(0, 1),
+      SUORCE_PATH,
+      TARGET_PATH,
+      encrypted_files => {
+        fs.stat(encrypted_files[0], (err, stats) => {
+          if (stats !== undefined) assert.equal(stats.isFile(), true);
+          done();
+        });
+      }
+    );
+  });
+
+  it("should encrypt two files", function(done) {
+    let gpg = initGPG("test@example.com");
+
+    encryptFiles(
+      gpg,
+      FILES.slice(0, 2),
+      SUORCE_PATH,
+      TARGET_PATH,
+      encrypted_files => {
+        assert.equal(
+          encrypted_files.reduce((a, f) => a + fs.statSync(f).isFile(), 0),
+          2
+        );
+        done();
+      }
+    );
+  });
+
+  it("should encrypt four files", function(done) {
+    let gpg = initGPG("test@example.com");
+
+    encryptFiles(
+      gpg,
+      FILES.slice(0, 4),
+      SUORCE_PATH,
+      TARGET_PATH,
+      encrypted_files => {
+        assert.equal(
+          encrypted_files.reduce((a, f) => a + fs.statSync(f).isFile(), 0),
+          4
+        );
+        done();
+      }
+    );
+  });
+
+  // it("should encrypt a single file in new-folder", function(done) {
+  //   let gpg = initGPG("test@example.com");
+
+  //   encryptFiles(
+  //     gpg,
+  //     FILES.slice(4),
+  //     SUORCE_PATH,
+  //     TARGET_PATH,
+  //     encrypted_files => {
+  //       fs.stat(encrypted_files[0], (err, stats) => {
+  //         if (stats !== undefined) assert.equal(stats.isFile(), true);
+  //         done();
+  //       });
+  //     }
+  //   );
+  // });
+});
