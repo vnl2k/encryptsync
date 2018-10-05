fa55f2914eafe643909955fcf2c06057a14989ff
vnl2k <38137627+vnl2k@users.noreply.github.com>
2018-10-05 16:41:17 +0100

    v 0.1.0: refactored encrypt js
    Added mocha tests

--- encrypt/start.js
+++ encrypt/start.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const { monitor } = require("./encrypt.js"),
+  EMAIL = process.argv[2],
+  DESTINATION_PATH = process.argv[4] || process.env.HOME + "/Dropbox",
+  SOURCE_PATH = process.argv[3] || process.cwd();
+
+if (EMAIL == undefined) {
+  console.log(
+    "Please provide recepient email, e.g.\n sudo npm run monitor john.smith@mail.com"
+  );
+  process.exit(1);
+}
+
+monitor(EMAIL, SOURCE_PATH, DESTINATION_PATH);
